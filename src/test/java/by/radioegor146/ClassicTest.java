58b375cbcb67d345fc89795278a49384e352b433
Egor Koleda <radioegor146@yandex.ru>
2019-04-30 21:42:24 +0300

    Update ClassicTest.java

--- src/test/java/by/radioegor146/ClassicTest.java
+++ src/test/java/by/radioegor146/ClassicTest.java
@@ -60,7 +60,7 @@ public class ClassicTest implements Executable {
                 throw new IOException("Source directory not found");
 
             System.out.println("Preparing...");
-            tempDirectory = Files.createTempDirectory("native-obfuscator-test");
+            tempDirectory = Files.createTempDirectory("native-obfuscator-test-N" + testDirectory.toFile().getName());
             Path tempSourceDirectory = tempDirectory.resolve("source");
             tempSourceDirectory.toFile().mkdirs();
             Path tempClassFilesDirectory = tempDirectory.resolve("classes");
@@ -116,12 +116,11 @@ public class ClassicTest implements Executable {
             }
             
             System.out.println("OK");
+            clean();
         } catch (IOException | RuntimeException e) {
             e.printStackTrace(System.err);
-            clean();
             throw e;
         }
-        clean();
     }
     
 }
