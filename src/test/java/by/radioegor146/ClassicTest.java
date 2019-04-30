793fadefeddeb7b3ae1f8dbd2a748cad747f032c
Egor Koleda <radioegor146@yandex.ru>
2019-04-30 22:53:28 +0300

    Update ClassicTest.java

--- src/test/java/by/radioegor146/ClassicTest.java
+++ src/test/java/by/radioegor146/ClassicTest.java
@@ -118,6 +118,7 @@ public class ClassicTest implements Executable {
             System.out.println("OK");
             clean();
         } catch (IOException | RuntimeException e) {
+            clean();
             e.printStackTrace(System.err);
             throw e;
         }
