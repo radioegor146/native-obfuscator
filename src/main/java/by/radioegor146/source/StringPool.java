32e05747f5e456f4632bdc7f6f815e51bfe6caee
radioegor146 <radioegor146@yandex.ru>
2019-05-05 16:24:40 +0300

    #21 fix

--- src/main/java/by/radioegor146/source/StringPool.java
+++ src/main/java/by/radioegor146/source/StringPool.java
@@ -24,13 +24,60 @@ public class StringPool {
         return String.format("((char *)(string_pool + %dLL))", pool.get(value));
     }
 
+    private static byte[] getModifiedUtf8Bytes(String str) {
+        int strlen = str.length();
+        int utflen = 0;
+        int c, count = 0;
+
+        for (int i = 0; i < strlen; i++) {
+            c = str.charAt(i);
+            if ((c >= 0x0001) && (c <= 0x007F)) {
+                utflen++;
+            } else if (c > 0x07FF) {
+                utflen += 3;
+            } else {
+                utflen += 2;
+            }
+        }
+
+        if (utflen > 65535)
+            throw new RuntimeException("encoded string too long: " + utflen + " bytes");
+
+        byte[] bytearr = new byte[utflen];
+
+        int i;
+        for (i = 0; i < strlen; i++) {
+           c = str.charAt(i);
+           if (!((c >= 0x0001) && (c <= 0x007F))) 
+               break;
+           bytearr[count++] = (byte) c;
+        }
+
+        for (; i < strlen; i++){
+            c = str.charAt(i);
+            if ((c >= 0x0001) && (c <= 0x007F)) {
+                bytearr[count++] = (byte) c;
+
+            } else if (c > 0x07FF) {
+                bytearr[count++] = (byte) (0xE0 | ((c >> 12) & 0x0F));
+                bytearr[count++] = (byte) (0x80 | ((c >>  6) & 0x3F));
+                bytearr[count++] = (byte) (0x80 | ((c) & 0x3F));
+            } else {
+                bytearr[count++] = (byte) (0xC0 | ((c >>  6) & 0x1F));
+                bytearr[count++] = (byte) (0x80 | ((c) & 0x3F));
+            }
+        }
+        
+        return bytearr;
+    }
+
     public String build() {
         List<Byte> bytes = new ArrayList<>();
         pool.entrySet().stream()
                 .sorted(Comparator.comparing(Map.Entry::getValue))
                 .map(Map.Entry::getKey)
                 .forEach(string -> {
-                    for (byte b : string.getBytes()) {
+                    for (byte b : getModifiedUtf8Bytes(string)) {
                         bytes.add(b);
                     }
                     bytes.add((byte) 0);
