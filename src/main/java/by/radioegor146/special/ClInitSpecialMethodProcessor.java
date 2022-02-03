99427921497843a25f8b93141fd74e06a5332775
mdxd44 <ogurec332@mail.ru>
2022-02-03 17:05:57 +0900

    Fix interfaces.

--- src/main/java/by/radioegor146/special/ClInitSpecialMethodProcessor.java
+++ src/main/java/by/radioegor146/special/ClInitSpecialMethodProcessor.java
@@ -11,10 +11,12 @@ public class ClInitSpecialMethodProcessor implements SpecialMethodProcessor {
     @Override
     public String preProcess(MethodContext context) {
         String name = "native_special_clinit" + context.methodIndex;
-        context.proxyMethod = new MethodNode(Opcodes.ASM7,
-                Opcodes.ACC_NATIVE | Opcodes.ACC_PRIVATE | Opcodes.ACC_STATIC | Opcodes.ACC_SYNTHETIC,
-                name, context.method.desc, context.method.signature, new String[0]);
-        context.clazz.methods.add(context.proxyMethod);
+        if (!Util.getFlag(context.clazz.access, Opcodes.ACC_INTERFACE)) {
+            context.proxyMethod = new MethodNode(Opcodes.ASM7,
+                    Opcodes.ACC_NATIVE | Opcodes.ACC_PRIVATE | Opcodes.ACC_STATIC | Opcodes.ACC_SYNTHETIC,
+                    name, context.method.desc, context.method.signature, new String[0]);
+            context.clazz.methods.add(context.proxyMethod);
+        }
         return name;
     }
 
@@ -26,18 +28,20 @@ public class ClInitSpecialMethodProcessor implements SpecialMethodProcessor {
         instructions.add(new LdcInsnNode(Type.getObjectType(context.clazz.name)));
         instructions.add(new MethodInsnNode(Opcodes.INVOKESTATIC, context.obfuscator.getNativeDir() + "/Loader",
                 "registerNativesForClass", "(ILjava/lang/Class;)V", false));
-        instructions.add(new MethodInsnNode(Opcodes.INVOKESTATIC, context.clazz.name,
-                "native_special_clinit" + context.methodIndex, context.method.desc, false));
 
         if (Util.getFlag(context.clazz.access, Opcodes.ACC_INTERFACE)) {
             if (context.nativeMethod == null) {
                 throw new RuntimeException("Native method not created?!");
             }
-            context.proxyMethod.instructions.add(new MethodInsnNode(Opcodes.INVOKESTATIC,
+            instructions.add(new MethodInsnNode(Opcodes.INVOKESTATIC,
                     context.obfuscator.getStaticClassProvider().getCurrentClassName(),
                     context.nativeMethod.name, context.nativeMethod.desc, false));
-            context.proxyMethod.instructions.add(new InsnNode(Opcodes.RETURN));
+            instructions.add(new InsnNode(Opcodes.RETURN));
+        } else {
+            instructions.add(new MethodInsnNode(Opcodes.INVOKESTATIC, context.clazz.name,
+                    "native_special_clinit" + context.methodIndex, context.method.desc, false));
         }
+
         instructions.add(new InsnNode(Opcodes.RETURN));
     }
 }
