fa72f870609d55ea11ec85ff2faeb70480f31dda
Egor Koleda <radioegor146@yandex.ru>
2024-02-05 23:34:32 +0300

    fix test

--- obfuscator/test_data/tests/clinit/TestClInitStacktrace/Test.java
+++ obfuscator/test_data/tests/clinit/TestClInitStacktrace/Test.java
@@ -1,9 +1,10 @@
 public class Test {
 
     private static void printStackTrace() {
+        String platform = System.getProperty("platform");
         StackTraceElement[] stack = new Exception().getStackTrace();
         for (int i = 0; i < stack.length; i++) {
-            if (System.getProperty("platform").equals("ANDROID") && stack[i].getClassName().equals("native0.hidden.Hidden0")) {
+            if ("ANDROID".equals(platform) && stack[i].getClassName().equals("native0.hidden.Hidden0")) {
                 continue;
             }
             System.out.printf("%d: %s.%s\n", i, stack[i].getClassName(), stack[i].getMethodName());
